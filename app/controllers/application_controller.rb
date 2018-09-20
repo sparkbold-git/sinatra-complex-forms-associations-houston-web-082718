be0e1bffaffa49234ebfe0787ef5f5e93eacdcb4
sparkbold-git <sparkboldstudio@gmail.com>
2018-09-20 09:08:27 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
+
+  get '/' do
+    "<h1>WELCOME TO PET-OWNER PAGE</h1>"
+  end
 end
